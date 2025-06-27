faf5ec39a6220536ebbc89aac0c374ed54f5b86d
AlanCapilla <118011904+AlanCapilla@users.noreply.github.com>
2025-06-27 13:00:04 -0600

    TsAgregado

--- Ts/index.ts
+++
@@ -1,4 +0,0 @@
-// codigo fuente en ts 
-//Segun el tutorial de https://www.youtube.com/watch?v=VXWvfrmpapI&list=PLYElE_rzEw_sHeIIv7BMliQF5zB7BliJE || Frank's Laboratory
-// se hace el siguiente codigo para graficar el audio, basado en Ts
-//Alan Capilla Guzman
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// codigo fuente en ts 
+//Segun el tutorial de https://www.youtube.com/watch?v=VXWvfrmpapI&list=PLYElE_rzEw_sHeIIv7BMliQF5zB7BliJE || Frank's Laboratory
+// se hace el siguiente codigo para graficar el audio, basado en Ts
+//Alan Capilla Guzman
+
+console.log("Hola, Graficacion");
